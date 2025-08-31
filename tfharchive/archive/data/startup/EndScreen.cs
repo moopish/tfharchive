aeb59327ac672b13f8eb64a2c800d3c8e9ff9f7a
moopish <michael.s.vandyk@gmail.com>
2025-08-31 11:13:53 -0400

    End screen text dump files now can be handled via the EndScreen class.

---
+++ tfharchive/archive/data/startup/EndScreen.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+using tfharchive.utilities.font;
+
+namespace tfharchive.archive.data.startup
+{
+    public class EndScreen : File
+    {
+        private const int CharacterTupleByteLength = 2;
+        private const int NibbleBitLength = 4;
+        private const int NibbleBitMask = 0x0F;
+
+        public const string FileDirectory = "STARTUP";
+        public const string FileExtension = "BIN";
+
+        private const int EndScreenLineLength = 80;
+
+        public const int EndScreenByteLength = 4000;
+
+        /// <summary>
+        /// Tracks the character, foreground, and background colour of what to print to screen.
+        /// </summary>
+        /// <param name="Character">The character to print.</param>
+        /// <param name="Foreground">The colour of the character.</param>
+        /// <param name="Background">The colour of the background where the character is drawn.</param>
+        private record struct CharacterTuple(char Character, int Foreground, int Background) { }
+
+        private readonly CharacterTuple[] _characters;
+
+        /// <summary>
+        /// Setup a given end screen with the given data.
+        /// </summary>
+        /// <param name="filename">The file name the end screen is found in.</param>
+        /// <param name="data">The data of the end screen file in bytes.</param>
+        public EndScreen(string filename, byte[] data) : base(filename)
+        {
+            CharacterTuple[] characterTuples = new CharacterTuple[EndScreenByteLength / CharacterTupleByteLength];
+
+            for (int i = 0; i < EndScreenByteLength; i += CharacterTupleByteLength)
+            {
+                int colourByte = data[i + 1];
+                int foreground = colourByte & NibbleBitMask;
+                int background = (colourByte >> NibbleBitLength) & NibbleBitMask;
+                char asciiByte = Codepage437.GetCharacter(data[i]);
+                characterTuples[i / CharacterTupleByteLength] = new CharacterTuple(asciiByte, foreground, background);
+            }
+
+            _characters = characterTuples;
+        }
+
+        public override string Directory => FileDirectory;
+
+        public override string Extension => FileExtension;
+
+        public override FileType FileType => FileType.EndScreen;
+
+        public override byte[] AsBytes()
+        {
+            return (byte[])_characters.Clone();
+        }
+
+        /// <summary>
+        /// Print the end screen to the console.
+        /// </summary>
+        public void PrintScreen()
+        {
+            Encoding original = Console.OutputEncoding;
+            Console.OutputEncoding = new UTF8Encoding(false);
+
+            int column = 0;
+
+            foreach (CharacterTuple tuple in _characters)
+            {
+                Console.BackgroundColor = (ConsoleColor)tuple.Background;
+                Console.ForegroundColor = (ConsoleColor)tuple.Foreground;
+
+                if (column == EndScreenLineLength)
+                {
+                    column = 0;
+                    Console.WriteLine();
+                }
+
+                ++column;
+                Console.Write(tuple.Character);
+            }
+
+            Console.OutputEncoding = original;
+        }
+    }
+}
---
+++ tfharchive/utilities/font/Codepage437.cs
@@ -0,0 +1,53 @@
+﻿namespace tfharchive.utilities.font
+{
+    internal static class Codepage437
+    {
+        /// <summary>
+        /// The characters use in the codepage 437.
+        /// </summary>
+        private static readonly char[] cp437Chars =
+        [
+            '\0','☺','☻','♥','♦','♣','♠','•','◘','○','◙',
+            '♂','♀','♪','♫','☼','►','◄','↕','‼','¶',
+            '§','▬','↨','↑','↓','→','←','∟','↔','▲','▼',
+
+            '\u00A0','!','"','#','$','%','&','\'','(',')',
+            '*','+',',','-','.','/','0','1','2',
+            '3','4','5','6','7','8','9',':',';','<',
+            '=','>','?','@','A','B','C','D','E','F',
+            'G','H','I','J','K','L','M','N','O','P',
+            'Q','R','S','T','U','V','W','X','Y','Z',
+            '[','\\',']','^','_','`','a','b','c','d',
+            'e','f','g','h','i','j','k','l','m','n',
+            'o','p','q','r','s','t','u','v','w','x',
+            'y','z','{','|','}','~','⌂',
+
+            'Ç','ü','é','â','ä','à','å','ç','ê','ë',
+            'è','ï','î','ì','Ä','Å','É','æ','Æ','ô',
+            'ö','ò','û','ù','ÿ','Ö','Ü','¢','£','¥',
+            '₧','ƒ','á','í','ó','ú','ñ','Ñ','ª','º',
+            '¿','⌐','¬','½','¼','¡','«','»',
+
+            '░','▒','▓','│','┤','╡','╢','╖','╕','╣',
+            '║','╗','╝','╜','╛','┐','└','┴','┬','├',
+            '─','┼','╞','╟','╚','╔','╩','╦','╠','═',
+            '╬','╧','╨','╤','╥','╙','╘','╒','╓','╫',
+            '╪','┘','┌','█','▄','▌','▐','▀','α','ß',
+            'Γ','π','Σ','σ','µ','τ','Φ','Θ','Ω','δ',
+            '∞','φ','ε','∩','≡','±','≥','≤','⌠','⌡',
+            '÷','≈','°','∙','·','√','ⁿ','²','■', '\u00A0'
+        ];
+
+        /// <summary>
+        /// Get the UTF-16 equivalent of the given codepage 437 character code.
+        /// </summary>
+        /// <param name="code">The code of the codepage 437 character.</param>
+        /// <returns>The UTF-16 character equivalent of the given codepage 437 character code.</returns>
+        /// <exception cref="ArgumentException">Thrown if the code is not in the range 0-255.</exception>
+        public static char GetCharacter(int code)
+        {
+            if (code < 0 || code >= cp437Chars.Length) throw new ArgumentException("Code is out of range: " + code);
+            return cp437Chars[code];
+        }
+    }
+}
